cb3d803a4b09a3588df08330129eb55364af44f7
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-07-05 10:11:15 +0530

    Assignment-8

--- RECURSION/Assignment_8/Question_7.java
+++ RECURSION/Assignment_8/Question_7.java
@@ -9,8 +9,47 @@ Example 1:
 Input: s = "3[a]2[bc]"
 Output: "aaabcbc"
  */
-class Question_7{
-    public static void main(String[] args) {
 
+import java.util.Stack;
+
+class Question_7 {
+    public static void main(String[] args) {
+        String s = "3[a]2[bc]";
+        System.out.println(decodeString(s)); //aaabcbc
+        }
+
+    public static String decodeString(String s) {
+        Stack<Integer> is = new Stack<>();
+        Stack<String> ss = new Stack<>();
+        int ptr = 0;
+        String res = "";
+        while (ptr < s.length()) {
+            char curr = s.charAt(ptr);
+            if (Character.isDigit(curr)) {
+                int num = 0;
+                // e.g. 32[a]
+                while (Character.isDigit(s.charAt(ptr))) {
+                    num = num * 10 + s.charAt(ptr) - '0';
+                    ptr++;
+                }
+                is.push(num); // Push the integer to Integer Stack
+            } else if (curr == '[') {
+                ss.push(res); // push the previous result
+                res = ""; // start a new string to record the further coming characters
+                ptr++;
+            } else if (curr == ']') {
+                StringBuilder sb = new StringBuilder(ss.pop());
+                int count = is.pop();
+                for (int i = 1; i <= count; i++) {
+                    sb.append(res);
+                }
+                res = sb.toString();
+                ptr++;
+            } else {
+                res += curr;
+                ptr++;
+            }
+        }
+        return res;
     }
 }
--- RECURSION/Assignment_8/Question_8.java
+++ RECURSION/Assignment_8/Question_8.java
@@ -8,8 +8,29 @@ Input: s = "ab", goal = "ba"
 Output: true
 Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
  */
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class Question_8{
     public static void main(String[] args) {
+        String s = "ab", goal = "ba";
+        System.out.println(buddyStrings(s,goal)); // true
+    }
+    public static boolean buddyStrings(String s, String goal) {
+        if(s.length() != goal.length()) return false;
 
+        if(s.equals(goal)){
+            Set<Character> set = new HashSet<Character>();
+            for(char c : s.toCharArray()) set.add(c);
+            return set.size() < s.length();
+        }
+        List<Integer> dif = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) != goal.charAt(i)) dif.add(i);
+        }
+        return dif.size() == 2 && s.charAt(dif.get(0)) == goal.charAt(dif.get(1)) && s.charAt(dif.get(1)) == goal.charAt(dif.get(0));
     }
 }
