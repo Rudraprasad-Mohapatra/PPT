51aa8ce66c213c7199fdd1cae77dac5cb872f0af
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-07-06 22:50:14 +0530

    Web Development-8

---
+++ WebDevelopment/web8/index.js
@@ -0,0 +1,13 @@
+const userOne = ()=>{
+    console.log("Hello One");
+}
+const userTwo = ()=>{
+    console.log("Hello Two");
+}
+const userThree = ()=>{
+    console.log("Hello Three");
+}
+
+userOne();
+userTwo();
+userThree();
