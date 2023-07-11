78cd07e9fb03c9440c1dd3488fc22ad498bd6d26
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-07-12 00:47:57 +0530

    Question-2 Completed

---
+++ WebDevelopment/Sunday_Assignment_Solution/Question_2/script.js
@@ -0,0 +1,9 @@
+document.getElementById('btn').addEventListener('click',()=>{
+    const inpVal = document.getElementsByTagName("input")[0].value;
+    if(inpVal !== ''){
+        const divi = document.createElement('div');
+        divi.innerHTML = inpVal;
+        document.body.appendChild(divi);
+        document.getElementsByTagName("input")[0].value = "";
+    }
+})
